c2a1c42a073874fbc7799466f38f537117421110
Stephanie Wilkinson <what.happens@gmail.com>
2017-02-01 19:36:05 -0800

    nests food inside of user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :foods
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :foods, shallow: true
+  end
 end
